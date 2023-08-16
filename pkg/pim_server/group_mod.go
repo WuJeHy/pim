a7a7daefd6f427e7351c108f489a04bf09b8630c
fan <leng_ju@outlook.com>
2023-08-16 09:07:49 +0800

    增加业务：通过群成员邀请新成员

--- pkg/pim_server/group_mod.go
+++ pkg/pim_server/group_mod.go
@@ -99,12 +99,55 @@ func (p *PimServer) GroupJoinByID(ctx context.Context, req *api.GroupJoinByIDReq
 		//}
 	}
 	// TODO 推送当前用户新聊天事件
+	resp = new(api.BaseOk)
 	return
 }
 
 func (p *PimServer) GroupInviteMembers(ctx context.Context, req *api.GroupInviteMembersReq) (resp *api.BaseOk, err error) {
-	//todo
-
+	// TODO 鉴权
+	// 鉴权失败
+	// 鉴权成功
+	// 基本类似GroupJoinByID
+	db := p.svr.db
+	logger := p.svr.logger
+	// 查找群
+	var group models.GroupBaseInfo
+	err = db.Where("group_id = ?", req.GroupID).Take(&group).Error
+	// 失败，return
+	if err != nil {
+		logger.Error("查无此群", zap.Int64("group_id", req.GroupID), zap.Int64("stream_id", req.StreamID))
+		return
+	}
+	// 添加群成员信息
+	var thisMembers []*models.UserInfoViewer
+	err = db.Where("user_id in ?", req.Members).Find(&thisMembers).Error
+	if err != nil {
+		logger.Error("添加群成员信息失败", zap.Int64("group_id", req.GroupID), zap.Int64("stream_id", req.StreamID))
+		return
+	}
+	var groupMembers []*models.GroupMember
+	if len(thisMembers) != 0 {
+		for _, am := range thisMembers {
+			temp := models.GroupMember{
+				GroupID:  group.GroupID,
+				MemberID: am.UserID,
+				Nick:     am.Nick,
+				//UserType: codes.GroupUserTypeNormal,
+			}
+			groupMembers = append(groupMembers, &temp)
+		}
+	}
+	_ = db.Create(&groupMembers)
+	// TODO 向新用户们推送新聊天事件
+	// 查找所有群成员
+	var allGroupMembers []*models.GroupMember
+	_ = db.Where("group_id = ?", req.GroupID).Find(&allGroupMembers).Error
+	// TODO 向所有成员推送"新人入群"通知
+	if len(allGroupMembers) != 0 {
+		//for _, m := range groupMembers{
+		//}
+	}
+	resp = new(api.BaseOk)
 	return
 }
 
